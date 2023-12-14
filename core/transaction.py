e01f2dd7f3fbd45065ccd4afcebbf0638e61c9f3
Nguyen-huan <huan06949@gmail.com>
2023-12-14 20:47:07 +0700

    add request transaction

--- core/transaction.py
+++ core/transaction.py
@@ -5,14 +5,19 @@ from django.shortcuts import render, redirect
 from account.models import Account
 from .models import Transaction
 
-@login_required
+@login_required 
 def get_transactions_list(request):
-    sender_transaction = Transaction.objects.filter(sender=request.user).order_by("-id")
-    receiver_transaction = Transaction.objects.filter(receiver=request.user).order_by("-id")
+    sender_transaction = Transaction.objects.filter(sender=request.user, transaction_type="transfer").order_by("-id")
+    reciever_transaction = Transaction.objects.filter(receiver=request.user, transaction_type="transfer").order_by("-id")
+    request_sender_transaction = Transaction.objects.filter(sender=request.user, transaction_type="request").order_by("-id")
+    request_reciever_transaction = Transaction.objects.filter(receiver=request.user, transaction_type="request").order_by("-id")
 
     context = {
         'sender_transaction':sender_transaction,
-        'reciever_transaction':receiver_transaction,
+        'reciever_transaction':reciever_transaction,
+        'request_sender_transaction':request_sender_transaction,
+        'request_reciever_transaction':request_reciever_transaction
+
 
     }
     return render(request, "transaction/transaction-list.html", context)
